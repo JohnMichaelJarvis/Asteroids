e71e1c96b8a95c97d0737a24095a31c6e31ac303
JohnMichaelJarvis <John_Michael_Jarvis@protonmail.com>
2025-12-16 17:25:20 -0500

    chore: Add circleshape.py; add framrate refresh to main.py

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
+from typing import Any
 import pygame
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
-pygame.init()
-screen  = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-
-
-def main():
-
+def main() -> None:
     pygame.init()
-    screen  = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
-    
+    screen: Any = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock: Any = pygame.time.Clock()
+    print(clock)
+    dt: int = 0
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
+    # Main game loop
     while True:
         log_state()
         for event in pygame.event.get():
@@ -25,7 +22,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
-
+        dt += clock.tick(60)
 
 
 if __name__ == "__main__":
