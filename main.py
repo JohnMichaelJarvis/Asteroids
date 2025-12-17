20a6c73818d166bd1020173ea6d70a60ab7c6b18
John Michael Jarvis <John_Michael_Jarvis@protonmail.com>
2025-12-17 19:11:00 +0000

    feat: implement Asteroid and AsteroidField classes with spawning logic and update methods

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from typing import Any
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, FPS
 
@@ -15,9 +17,13 @@ def main() -> None:
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
 
+    asteroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
