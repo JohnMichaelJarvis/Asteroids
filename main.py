5d7e1d7e644fb74e45a7df0798744c69c6f0e12a
JohnMichaelJarvis <John_Michael_Jarvis@protonmail.com>
2025-12-17 20:11:35 -0500

    Add shooting mechanics and implement Shot class
    - Introduced a new Shot class to handle projectile behavior.
    - Updated Player class to allow shooting with the spacebar.
    - Integrated Shot class into the main game loop for proper rendering and updating.
    - Adjusted constants to include PLAYER_SHOOT_SPEED for shot velocity.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from typing import Any
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 from logger import log_state, log_event
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, FPS
 
@@ -19,10 +20,12 @@ def main() -> None:
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
     Asteroid.containers = (updatable, drawable, asteroids)
     AsteroidField.containers = updatable
+    Shot.containers = (updatable, drawable)
 
     asteroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
--- player.py
+++ player.py
@@ -1,6 +1,13 @@
 import pygame
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_SPEED, PLAYER_TURN_SPEED
+from constants import (
+    PLAYER_RADIUS,
+    LINE_WIDTH,
+    PLAYER_SHOOT_SPEED,
+    PLAYER_SPEED,
+    PLAYER_TURN_SPEED,
+)
 from circleshape import CircleShape
+from shot import Shot
 
 
 class Player(CircleShape):
@@ -40,9 +47,15 @@ class Player(CircleShape):
             self.move(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
 
     def move(self, dt: int) -> None:
         unit_vector = pygame.Vector2(0, 1)
         rotated_vector = unit_vector.rotate(self.rotation)
         rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
         self.position += rotated_with_speed_vector
+
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y, 1)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
